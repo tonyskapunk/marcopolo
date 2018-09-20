abbb04c713cf754121495f350e00e06a48781593
Greg Swift <gregswift@gmail.com>
2018-09-20 11:57:04 -0500

    Get parse to pull from environment file

--- marcopolo/cli.py
+++ marcopolo/cli.py
@@ -1,9 +1,10 @@
+import os
 import sys
 from objects import parse
 
 def parse_schema_file(input=None):
     if input is None:
-        input = sys.argv[1]
+        input = os.environ.get('POLO_FILE')
     polo = parse(open(input, 'r').read())
     print polo.to_yaml()
     return polo
