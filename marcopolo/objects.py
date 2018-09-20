11b2cc538bcab40badfa6066223be51b1adb566d
matt1757 <matthew.manning@rackspace.com>
2018-09-20 15:56:55 -0500

    womp womp!

--- marcopolo/objects.py
+++ marcopolo/objects.py
@@ -109,10 +109,10 @@ class Polo(SerializableObject):
         self.environments = []
         for e in kwargs.get('environments', {}):
             env = Environment(**e)
-            if not kwargs.get('name'):
+            if not kwargs.get('name') and not kwargs.get('tier'):
                 continue
 
-            env.set_name(self, name=kwargs['name'])
+            env.set_name(self, name=kwargs['name'] + '.' +kwargs['tier'])
             env.create_nodes()
             self._targets.append(env.name)
             self._targets.extend(env.aliases)
